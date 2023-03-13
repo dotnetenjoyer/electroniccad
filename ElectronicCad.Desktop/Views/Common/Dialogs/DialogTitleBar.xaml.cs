d3223e29b73c9bdd76bb2a5bb0528a7b8f37eb22
Alexey Shchagolchin <lesha.shhagolchin@mail.ru>
2023-03-13 20:56:31 +0700

    feat add dialog title bar

--- ElectronicCad.Desktop/Views/Common/Dialogs/DialogTitleBar.xaml.cs
+++ ElectronicCad.Desktop/Views/Common/Dialogs/DialogTitleBar.xaml.cs
@@ -1,9 +1,50 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ElectronicCad.Desktop.Views.Common.Dialogs;
 
-public partial class DialogTitleBar : UserControl
+/// <summary>
+/// Dialog title bar control.
+/// </summary>
+internal partial class DialogTitleBar : UserControl
 {
+    /// <summary>
+    /// Title.
+    /// </summary>
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty); 
+        set => SetValue(TitleProperty, value);
+    }
+    
+    /// <summary>
+    /// Title dependecy property
+    /// </summary>
+    public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+        nameof(Title),
+        typeof(string),
+        typeof(DialogTitleBar), 
+        new PropertyMetadata());
+
+    /// <summary>
+    /// Close dialog command.
+    /// </summary>
+    public ICommand CloseCommand
+    {
+        get => (ICommand) GetValue(CloseCommandProperty); 
+        set => SetValue(CloseCommandProperty, value);
+    }
+
+    /// <summary>
+    /// Close dialog command dependency property.
+    /// </summary>
+    public static readonly DependencyProperty CloseCommandProperty = DependencyProperty.Register(
+        nameof(CloseCommand),
+        typeof(ICommand),
+        typeof(DialogTitleBar),
+        new PropertyMetadata());
+    
     public DialogTitleBar()
     {
         InitializeComponent();
