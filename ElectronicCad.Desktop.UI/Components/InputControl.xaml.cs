833de2cdbea434bbba45fa70e667740c1cbb5ba6
Alexey Shchagolchin <lesha.shhagolchin@mail.ru>
2023-03-23 21:22:17 +0700

    add new button styles and start implementing custom input control

---
+++ ElectronicCad.Desktop.UI/Components/InputControl.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ElectronicCad.Desktop.Styles.Components
+{
+    /// <summary>
+    /// Interaction logic for u.xaml
+    /// </summary>
+    public partial class InputControl : UserControl
+    {
+        /// <summary>
+        /// Title.
+        /// </summary>
+        public string Title 
+        { 
+            get => (string)GetValue(TitleProperty); 
+            set => SetValue(TitleProperty, value);
+        }
+
+        private static DependencyProperty TitleProperty = 
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(InputControl),
+                new PropertyMetadata());
+
+        /// <summary>
+        /// Placeholder.
+        /// </summary>
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        private static DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register(
+                nameof(Placeholder),
+                typeof(string),
+                typeof(InputControl),
+                new PropertyMetadata());
+
+        /// <summary>
+        /// Value.
+        /// </summary>
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        private static DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(string),
+                typeof(InputControl),
+                new FrameworkPropertyMetadata
+                {
+                    BindsTwoWayByDefault = true,
+                    DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                });
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public InputControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
