4e4d742f56a1329b012b2d2226455c152dcb9f93
Alexey Shchagolchin <lesha.shhagolchin@mail.ru>
2023-05-11 02:13:18 +0700

    add string format to input control

---
+++ ElectronicCad.Desktop.UI/Converters/ValueFormatMultipleConverter.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ElectronicCad.Desktop.UI.Converters;
+
+/// <summary>
+/// The converter that allow you to apply a format to a value,
+/// order in the multiple binding is as folows [value, format]
+/// </summary>
+public class ValueFormatMultipleConverter : IMultiValueConverter
+{
+    /// <inhertidoc />
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var value = (string) values[0];
+        var format = (string) values[1];
+
+        if (!string.IsNullOrEmpty(format))
+        {
+            if (float.TryParse(value, out var floatValue))
+            {
+                if (value.EndsWith(".") || value.EndsWith("0"))
+                {
+                    return value;
+                }
+
+                return string.Format(format, floatValue);
+            }
+        }
+
+        return value;
+    }
+
+    /// <inhertidoc />
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return new[] { value };
+    }
+}
