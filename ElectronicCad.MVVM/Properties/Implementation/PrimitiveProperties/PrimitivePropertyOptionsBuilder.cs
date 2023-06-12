762d5bab43e4f1d04ab3712d5610f714a832aca0
Alexey Shchagolchin <lesha.shhagolchin@mail.ru>
2023-06-13 06:44:58 +0700

    add primitive proeprty options builder EC-29

---
+++ ElectronicCad.MVVM/Properties/Implementation/PrimitiveProperties/PrimitivePropertyOptionsBuilder.cs
@@ -0,0 +1,37 @@
+﻿namespace ElectronicCad.MVVM.Properties.Implementation.PrimitiveProperties;
+
+/// <summary>
+/// Builder for primitive property options.
+/// </summary>
+public class PrimitivePropertyOptionsBuilder
+{
+    private readonly PrimitivePropertyOptions configuration;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public PrimitivePropertyOptionsBuilder()
+    {
+        configuration = new();
+    }
+
+    /// <summary>
+    /// Creates primitive property options.
+    /// </summary>
+    /// <returns>Primitive proeprty options.</returns>
+    public PrimitivePropertyOptions Build()
+    { 
+        return configuration;
+    }
+
+    /// <summary>
+    /// Specify primitive property name.
+    /// </summary>
+    /// <param name="name">Name.</param>
+    /// <returns>Builder</returns>
+    public PrimitivePropertyOptionsBuilder HasName(string name)
+    {
+        configuration.Name = name;
+        return this;
+    }
+}
